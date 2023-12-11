1ca346d0b3e6521d5c27fb54024f9aa6baebbd2d
Programador Cego <programadorcego@gmail.com>
2023-12-11 07:29:57 -0300

    Aula 02: readystatechange

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.querySelector(".button").addEventListener("click", () => {
     const request = new XMLHttpRequest();
     request.open("GET", "http://localhost/programadorcego/javascript/ajax/arquivo.txt", true);
-    request.onload = () => {
-        if(request.status === 200){
+    request.onreadystatechange = () => {
+        if(request.readyState === 4 && request.status === 200){
             document.querySelector("#text").textContent = request.response;
         }
+
+        //console.log(request.readyState);
     };
 
     request.send();
