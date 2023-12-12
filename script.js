e038961d27a8e0fa3b60795c6202bf4f75004a26
Programador Cego <programadorcego@gmail.com>
2023-12-12 07:12:23 -0300

    Aula 06: API Externa

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 document.querySelector(".button").addEventListener("click", () => {
     const request = new XMLHttpRequest();
-    request.open("GET", "http://localhost/programadorcego/javascript/ajax/dados.json", true);
+    request.open("GET", "http://localhost/api", true);
 
     request.onload = () => {
-        if(request.status === 200) {
+        if(request.status === 200){
             let dados = JSON.parse(request.response);
-            document.querySelector("tbody").innerHTML = "";
-
-            dados.forEach(dado => {
-            const tr = document.createElement("tr")    ;
-            const tdId = document.createElement("td");
-            const tdName = document.createElement("td");
-            const tdEmail = document.createElement("td");
-
-            tdId.textContent = dado.id;
-            tdName.textContent = dado.name;
-            tdEmail.textContent = dado.email;
-
-            tr.append(tdId, tdName, tdEmail);
-            document.querySelector("tbody").appendChild(tr);
-            });
+            console.log(dados[0].nome)
         }
-    };
+    }
 
     request.send();
+
 });
