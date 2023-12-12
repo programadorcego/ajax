9f53d8aa17ebe6109d79e87d0fe580834b045fc7
Programador Cego <programadorcego@gmail.com>
2023-12-12 06:47:24 -0300

    Aula 04: Criando HTML

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 document.querySelector(".button").addEventListener("click", () => {
     const request = new XMLHttpRequest();
     request.open("GET", "http://localhost/programadorcego/javascript/ajax/dados.json", true);
-    request.onload = () => {
-        if(request.status === 200){
-            const dados = JSON.parse(request.response);
-            console.log(dados.name);
-        }
 
-        //console.log(request.readyState);
+    request.onload = () => {
+        if(request.status === 200) {
+            let dados = JSON.parse(request.response);
+            document.querySelector("tbody").innerHTML = "";
+
+            dados.forEach(dado => {
+            const tr = document.createElement("tr")    ;
+            const tdId = document.createElement("td");
+            const tdName = document.createElement("td");
+            const tdEmail = document.createElement("td");
+
+            tdId.textContent = dado.id;
+            tdName.textContent = dado.name;
+            tdEmail.textContent = dado.email;
+
+            tr.append(tdId, tdName, tdEmail);
+            document.querySelector("tbody").appendChild(tr);
+            });
+        }
     };
 
     request.send();
