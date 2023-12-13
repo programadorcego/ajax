d4e9617795074dc7264a5646ac1d698c6a690c16
Programador Cego <programadorcego@gmail.com>
2023-12-13 07:09:03 -0300

    Aula 08: Promises

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
 document.querySelector(".button").addEventListener("click", () => {
-    const request = new XMLHttpRequest();
-    request.open("GET", "https://pokeapi.co/api/v2/pokemon", true);
+    let promessa = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("Olá, mundo!"), 10000);
+    });
 
-    request.onload = () => {
-        document.querySelector("tbody").innerHTML = "";
-
-        if(request.status === 200) {
-            const response = JSON.parse(request.response);
-            
-            response.results.forEach((pokemon, index) => {
-                const tr = document.createElement("tr");
-                const tdId = document.createElement("td");
-                const tdName = document.createElement("td");
-
-                tdId.textContent = index + 1;
-                tdName.textContent = pokemon.name;
-
-                tr.append(tdId, tdName);
-                document.querySelector("tbody").appendChild(tr);
-            });
-        }
-    };
-
-    request.send();
+    console.log(promessa);
 });
