d788f6b9561b8b61b5f252b715bb8af57087ad4c
Programador Cego <programadorcego@gmail.com>
2023-12-13 06:49:55 -0300

    Aula 07: Testando API Pokemon

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 document.querySelector(".button").addEventListener("click", () => {
     const request = new XMLHttpRequest();
-    request.open("GET", "http://localhost/api", true);
+    request.open("GET", "https://pokeapi.co/api/v2/pokemon", true);
 
     request.onload = () => {
-        if(request.status === 200){
-            let dados = JSON.parse(request.response);
-            console.log(dados[0].nome)
+        document.querySelector("tbody").innerHTML = "";
+
+        if(request.status === 200) {
+            const response = JSON.parse(request.response);
+            
+            response.results.forEach((pokemon, index) => {
+                const tr = document.createElement("tr");
+                const tdId = document.createElement("td");
+                const tdName = document.createElement("td");
+
+                tdId.textContent = index + 1;
+                tdName.textContent = pokemon.name;
+
+                tr.append(tdId, tdName);
+                document.querySelector("tbody").appendChild(tr);
+            });
         }
-    }
+    };
 
     request.send();
-
 });
