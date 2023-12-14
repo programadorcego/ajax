e274fecfd82340d1ce9b3f0b7e5d6ecf890f9adf
Programador Cego <programadorcego@gmail.com>
2023-12-14 07:21:44 -0300

    Aula 12: Introdução ao fetch API

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
-function promessa() {
-    return new Promise((resolve, reject) => {
-        let numero = Math.floor(Math.random() * (11 - 1)) + 1;
-
-        if(numero % 2 == 1) return reject(`Erro! O número ${numero} é ímpar.`);
-
-        return resolve(`O número ${numero} é par.`);
-    });
-}
-
 document.querySelector(".button").addEventListener("click", () => {
-console.clear();
-
-    promessa()
-.then(resultado => console.log(resultado))
-.catch(erro => console.error(erro))
-.finally(() => console.log("Fim da Promise"));
+    fetch("http://localhost/api/text")
+    .then(resultado => resultado.text())
+    .then(texto => {
+        document.querySelector("#text").textContent = texto;
+    });
 });
